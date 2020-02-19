63b3fda2f5b57619c63668130662aa78287a8a8b
Mark Grigoryev <mark31grig@gmail.com>
2020-02-19 19:23:47 +0500

    feat: search controller with no logic, just test

---
+++ weather-widget-backend/Controllers/SearchController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Cors;
+
+namespace weather_widget_backend.Controllers
+{
+    [Route("api/[controller]")]
+    [EnableCors("MyPolicy")]
+    [ApiController]
+    public class SearchController : ControllerBase
+    {
+        // GET: api/Search
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Search/searchString
+        [HttpGet("{searchString}", Name = "Get")]
+        public string Get(string searchString)
+        {
+            return searchString;
+        }
+    }
+}
